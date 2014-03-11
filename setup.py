681d458178be5a46bf9b7d598477d71d540ceb20
root <root@SOLR4-1.(none)>
2014-03-11 09:21:16 +0000

    ibetter setup

--- setup.py
+++ setup.py
@@ -7,18 +7,24 @@ from os.path import expanduser
 home = expanduser("~")
 
 def start(args, logfile, errfile):
-  setup_util.replace_text("/home/solr/benchmarks/platypus/src/resources/application.conf", "db.default.host=shopping", "db.default.host=" + args.database_host)
-  setup_util.replace_text("/home/solr/benchmarks/platypus/src/resources/application.conf", "db.default.user=root", "db.default.user=benchmarkdbuser")
-  setup_util.replace_text("/home/solr/benchmarks/platypus/src/resources/application.conf", "db.default.password=myroot", "db.default.password=benchmarkdbpass")
+  print("----point1----")
+  setup_util.replace_text("/home/solr/benchmarks/platypus/src/main/resources/application.conf", "db.default.host=shopping", "db.default.host=" + args.database_host)
+  print("----point2----")
+  setup_util.replace_text("/home/solr/benchmarks/platypus/src/main/resources/application.conf", "db.default.user=root", "db.default.user=benchmarkdbuser")
+  print("----point4----")
+  setup_util.replace_text("/home/solr/benchmarks/platypus/src/main/resources/application.conf", "db.default.password=myroot", "db.default.password=benchmarkdbpass")
 
   try:
-    subprocess.check_call("cd platypus && sbt runPlatypus", shell=True, stderr=errfile, stdout=logfile)
+    os.chdir("/home/solr/benchmarks/platypus")
+    subprocess.check_call("sbt runPlatypus", shell=True, stderr=errfile, stdout=logfile)
+    print("the platypus has been invoked")
     return 0
   except subprocess.CalledProcessError:
     return 1
 
 def stop(logfile, errfile):
   try:
+    os.chdir("/home/solr/benchmarks/platypus")
     subprocess.check_call("cd platypus && sbt stopPlatypus", shell=True, stderr=errfile, stdout=logfile)
     return 0
   except subprocess.CalledProcessError:
